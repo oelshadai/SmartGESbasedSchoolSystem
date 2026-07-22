b16252a38e110da3d430708f489b9750b3d47b37
Osei Elshaadai <oseielshadai8@gmail.com>
2026-07-22 08:54:11 +0000

    Fix migration conflict: merge 0006_smslog, 0006_supportticket_priority_reply, 0007_pushsubscription

---
+++ backend/notifications/migrations/0008_merge_migrations.py
@@ -0,0 +1,13 @@
+from django.db import migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('notifications', '0006_smslog'),
+        ('notifications', '0006_supportticket_priority_reply'),
+        ('notifications', '0007_pushsubscription'),
+    ]
+
+    operations = [
+    ]
